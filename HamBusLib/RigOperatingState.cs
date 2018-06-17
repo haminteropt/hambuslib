f7588d465ca5f7ce45bb56716f33334c07067249
DELENN\wa1go <darryl@hambus.org>
2018-06-17 13:05:02 -0500

    added RigOperatingState

---
+++ HamBusLib/RigOperatingState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HamBusLib
+{
+    public class RigOperatingState : UdpCmdPacket
+    {
+        public int Freq { get; set; }
+        public string Mode { get; set; }
+    }
+}
