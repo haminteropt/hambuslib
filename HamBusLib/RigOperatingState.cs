f06f68c5b27b298e56f245587192f00af2547a81
DELENN\wa1go <darryl@hambus.org>
2018-06-27 20:42:25 -0500

    refactor names to naming convention.

--- HamBusLib/RigOperatingState.cs
+++ HamBusLib/RigOperatingState.cs
@@ -7,9 +7,15 @@ namespace HamBusLib
     public class RigOperatingState : UdpCmdPacket
     {
         public Int64 Freq { get; set; }
+        public Int64 FreqA { get; set; }
+        public Int64 FreqB { get; set; }
         public string Mode { get; set; }
         public string ModeStd { get; set; }
         public bool Tx { get; set; }
+        public string Vfo { get; set; }
+        public string Xit { get; set; }
+        public string Split { get; set; }
+        public string Rit { get; set; }
 
         public RigOperatingState()
         {
