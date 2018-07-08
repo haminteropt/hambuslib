cfd9b240dd66a16b75b70721b5aff625b878ccfe
DELENN\wa1go <darryl@hambus.org>
2018-07-07 19:29:47 -0500

    added com port to reporting class

--- HamBusLib/RigBusInfo.cs
+++ HamBusLib/RigBusInfo.cs
@@ -10,6 +10,7 @@ namespace HamBusLib
         public string RigType { get; set; }
         public bool SendSyncInfo { get; set; } = false;
         public bool HonorTx { get; set; } = false;
+        public string ComPort { get; set; }
 
         public RigBusInfo()
         {
