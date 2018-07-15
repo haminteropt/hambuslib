a587c29709ff9437dabfda4f96076de5247d02b0
DELENN\wa1go <darryl@hambus.org>
2018-07-14 21:43:30 -0500

    Starting of generic json parsing

--- HamBusLib/DirectoryClient.cs
+++ HamBusLib/DirectoryClient.cs
@@ -1,20 +1,36 @@
-﻿using HamBusLib.Models;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace HamBusLib
+﻿namespace HamBusLib
 {
+    using HamBusLib.Models;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net.Http;
+    using System.Threading;
+
+    /// <summary>
+    /// Defines the <see cref="DirectoryClient" />
+    /// </summary>
     public class DirectoryClient
     {
+        /// <summary>
+        /// Defines the Instance
+        /// </summary>
         static public readonly DirectoryClient Instance = new DirectoryClient();
+
+        /// <summary>
+        /// Defines the threadRunning
+        /// </summary>
         private bool threadRunning = false;
+
+        /// <summary>
+        /// Defines the serviceInfoThread
+        /// </summary>
         private Thread serviceInfoThread;
 
+        /// <summary>
+        /// The StartThread
+        /// </summary>
         public void StartThread()
         {
             if (threadRunning == false)
@@ -24,7 +40,10 @@ namespace HamBusLib
             }
         }
 
-        private async  void PollDirectoryBus()
+        /// <summary>
+        /// The PollDirectoryBus
+        /// </summary>
+        private async void PollDirectoryBus()
         {
             threadRunning = true;
             try
@@ -38,18 +57,83 @@ namespace HamBusLib
                     {
                         var response = await httpClient.GetStringAsync(new Uri(url));
                         Console.WriteLine("Json: {0}", response);
-                        var root = JsonConvert.DeserializeObject<dynamic>(response);
+                        //var root = JsonConvert.DeserializeObject<dynamic>(response);
+                        var buses = JsonConvert.DeserializeObject<List<UdpCmdPacket>>(response);
+                        parseJson(response);
                         Thread.Sleep(HamBusEnv.SleepTimeMs);
                     }
                 }
-            } catch (Exception e)
+            }
+            catch (Exception e)
             {
                 Console.WriteLine("PollDirectoryBus exceptions: {0}", e.Message);
                 threadRunning = false;
             }
-
-
         }
 
+        /// <summary>
+        /// The parseJson
+        /// </summary>
+        /// <param name="response">The response<see cref="string"/></param>
+        private static void parseJson(string response)
+        {
+
+            List<JsonBase> busList = new List<JsonBase>();
+            JsonBase node = new JsonBase();
+            string propName = "";
+
+
+            JsonTextReader reader = new JsonTextReader(new StringReader(response));
+            while (reader.Read())
+            {
+                switch (reader.TokenType)
+                {
+                    case JsonToken.StartArray:
+                        break;
+                    case JsonToken.StartObject:
+                        break;
+                    case JsonToken.Integer:
+                        node = new JsonNode<int>();
+                        (node as JsonNode<int>).value = int.Parse(reader.Value.ToString());
+                        node.PropName = propName;
+                        busList.Add(node);
+                        break;
+                    case JsonToken.Float:
+                        node = new JsonNode<float>();
+                        (node as JsonNode<float>).value = float.Parse(reader.Value.ToString());
+                        node.PropName = propName;
+                        busList.Add(node);
+                        break;
+                    case JsonToken.String:
+                        node = new JsonNode<string>();
+                        (node as JsonNode<string>).value = reader.Value.ToString();
+                        node.PropName = propName;
+                        busList.Add(node);
+                        break;
+                    case JsonToken.Date:
+                        node = new JsonNode<DateTime>();
+                        node.PropName = propName;
+                        (node as JsonNode<DateTime>).value = DateTime.Parse(reader.Value.ToString());
+                        busList.Add(node);
+                        break;
+                    case JsonToken.EndObject:
+                        break;
+                    case JsonToken.PropertyName:
+                        propName = reader.Value.ToString();
+                        break;
+
+                }
+
+                node = null;
+                if (reader.Value != null)
+                {
+                    Console.WriteLine("Token: {0}, Value: {1}", reader.TokenType, reader.Value);
+                }
+                else
+                {
+                    Console.WriteLine("Token: {0}", reader.TokenType);
+                }
+            }
+        }
     }
 }
---
+++ HamBusLib/Models/JsonNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HamBusLib.Models
+{
+    public class JsonBase
+    {
+        public string name { get; set; }
+        public string PropName { get; set; }
+    }
+    public class JsonNode<T> : JsonBase
+    {
+
+        public T value { get; set; }
+        public JsonNode() { }
+    }
+}
