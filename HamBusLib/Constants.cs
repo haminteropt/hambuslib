d0dcbc06882823204cd78482122950ec03522da6
Darryl Wagoner <darryl.wagoner@statellite.com>
2019-03-27 18:34:08 -0500

    misc check in

--- HamBusLib/Constants.cs
+++ HamBusLib/Constants.cs
@@ -7,5 +7,7 @@ namespace HamBusLib
     public class Constants
     {
         public const int DirPortUdp = 7300;
+        public const string VirtualRigName = "VirtualRig";
+        public const string VirtualRigType = "Virtual";
     }
 }
